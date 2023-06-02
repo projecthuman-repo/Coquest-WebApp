445b233fe8f991957d1e81f1d43a22c543a25dff
JosephIsedowo <61061657+JosephIsedowo@users.noreply.github.com>
2023-06-01 20:12:57 -0400

    Delete Regenquest GraphQL API directory

--- Regenquest GraphQL API/models/regenquestGenres.js
+++
@@ -1,8 +0,0 @@
-const { model, Schema } = require("mongoose");
-
-//list of availible genres
-const regenquestGenresSchema = new Schema({
-  genre: [String],
-});
-
-module.exports = model("regenquestGenres", regenquestGenresSchema);
