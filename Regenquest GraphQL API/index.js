442df3b03607784640c75172f53d265b6febe15a
Yuta Kusuno <yutakusuno51@gmail.com>
2024-02-13 18:41:31 -0800

    Setup crossPlatform db and user

--- Regenquest GraphQL API/index.js
+++ Regenquest GraphQL API/index.js
@@ -1,28 +1,28 @@
-const { ApolloServer, gql } = require("apollo-server-cloud-functions");
+const { ApolloServer, gql } = require('apollo-server-cloud-functions');
 const {
   ApolloServerPluginLandingPageLocalDefault,
-} = require("apollo-server-core");
-
-const mongoose = require("mongoose");
-require("dotenv").config();
+} = require('apollo-server-core');
+const { connectToDatabases } = require('./db/connection');
 
 // Construct a schema, using GraphQL schema language
-const typeDefs = require("./graphql/typeDefs");
+const typeDefs = require('./graphql/typeDefs');
 
 // Provide resolver functions for your schema fields
-const resolvers = require("./graphql/resolvers");
+const resolvers = require('./graphql/resolvers');
 
-const MONGODB = process.env.MONGODB;
-
-mongoose.connect(MONGODB, { useNewUrlParser: true }).then(() => {
-  console.log("mongodb connection successful.");
-});
+connectToDatabases()
+  .then(() => {
+    console.log('All mongodb connection successful.');
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   csrfPrevention: true,
-  cache: "bounded",
+  cache: 'bounded',
   plugins: [ApolloServerPluginLandingPageLocalDefault({ embed: true })],
 });
 
