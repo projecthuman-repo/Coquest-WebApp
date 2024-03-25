c8f9a9da465e8f4ffe402d774c501b730f972a8f
Karanvir0dh <137131993+Karanvir0dh@users.noreply.github.com>
2024-03-25 17:06:35 -0400

    added comments

--- Regenquest GraphQL API/models/regenquestMotive.js
+++ Regenquest GraphQL API/models/regenquestMotive.js
@@ -1,5 +1,6 @@
 const { Schema, model } = require("mongoose");
 
+// name: name of the motive
 const regenquestMotiveSchema = new Schema({
   name: { type: String, required: true, unique: true }
 });
--- Regenquest GraphQL API/models/regenquestTopic.js
+++ Regenquest GraphQL API/models/regenquestTopic.js
@@ -1,5 +1,6 @@
 const { Schema, model } = require("mongoose");
 
+// name: name of the topic
 const regenquestTopicSchema = new Schema({
   name: { type: String, required: true, unique: true }
 });
