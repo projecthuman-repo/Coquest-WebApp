d649d3eba9a76917689af8f185d5546b4dbb94bc
Nicholas Defranco <defrann8208@outlook.com>
2024-07-30 17:31:00 -0400

    gcloud prints

--- Regenquest GraphQL API/utils/gcloud.js
+++ Regenquest GraphQL API/utils/gcloud.js
@@ -3,11 +3,10 @@ const client = new SecretManagerServiceClient({
   keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS,
 });
 
-console.log('credentials path:', process.env.GOOGLE_APPLICATION_CREDENTIALS);
-
 async function getSecret(secretName) {
   const name = `projects/base-map-workspace/secrets/${secretName}/versions/latest`;
-  console.log('accessing some secret version...');
+  console.log('accessing some secret version of', name);
+  console.log('credentials path:', process.env.GOOGLE_APPLICATION_CREDENTIALS);
 
   const [version] = await client.accessSecretVersion({ name });
 
