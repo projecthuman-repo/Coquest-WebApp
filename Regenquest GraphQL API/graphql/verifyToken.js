96dfc2411538a914b65e58ad23e692a46b4d930a
Nicholas Defranco <defrann8208@outlook.com>
2024-07-30 17:18:57 -0400

    verify token prints

--- Regenquest GraphQL API/graphql/verifyToken.js
+++ Regenquest GraphQL API/graphql/verifyToken.js
@@ -9,11 +9,13 @@ class VerifyTokenDirective extends SchemaDirectiveVisitor {
     field.resolve = async function (...args) {
       const [, , context] = args;
       // Extract the token from arguments
+      console.log('fdusfsdf');
       const token = args[1].token; // assuming the token is the second argument
+      console.log('token in verify token:', token);
       const secret = await getSecret(process.env.ACCESS_JWT_NAME);
-
+      console.log('Got secret');
       await verifyToken(token, secret, context);
-
+      console.log('verified token!');
       return originalResolve.apply(this, args);
     };
   }
