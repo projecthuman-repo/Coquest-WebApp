9bb010b2026759753fd9e285b3c9a8eae2b64ed3
pranav-suri <pranavsuri4@gmail.com>
2024-09-13 23:27:31 +0530

    fix: response delay reduced and added some types
    jwt_secret key was fetched from GCP at every request causing a delay of about 450ms, now it is fetched only the applicatoin startup effectively eliminating this delay during queries.

--- backend/src/utils/gcloud.ts
+++ backend/src/utils/gcloud.ts
@@ -1,13 +1,23 @@
 import { SecretManagerServiceClient } from "@google-cloud/secret-manager";
+import CONFIG from "../config";
 const client = new SecretManagerServiceClient({
   keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS,
 });
 
+// TODO: Implement proper cache invalidation, this is just a makeshift solution
+const secretsPromise = client.accessSecretVersion({
+  name: `projects/base-map-workspace/secrets/${CONFIG.ACCESS_JWT_NAME}/versions/latest`,
+});
+
 export async function getSecret(secretName: string) {
   const name = `projects/base-map-workspace/secrets/${secretName}/versions/latest`;
-
-  const [version] = await client.accessSecretVersion({ name });
-  const payload = version.payload?.data?.toString();
+  let [version] = await secretsPromise;
+  // As of writing this comment, the only secret that was fetched across all files was ACCESS_JWT_NAME
+  // This condition was written in case the function is used for fetching other secrets.
+  if (secretName !== CONFIG.ACCESS_JWT_NAME) {
+    [version] = await client.accessSecretVersion({ name });
+  }
+  const payload = version?.payload?.data?.toString();
 
   return payload;
 }
